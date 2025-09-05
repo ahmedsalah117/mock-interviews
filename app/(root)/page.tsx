67b7f42e2551d55623fda1d8817857aa10d09c6f
Ahmed Bahnasy <engineerahmedbahnasy@gmail.com>
2025-09-05 18:56:21 +0300

    feat: shows generated interviews on the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
 import InterviewCard from "@/components/InterviewCard";
 import { Button } from "@/components/ui/button";
-import { dummyInterviews } from "@/constants";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import {
+  getInterviewByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/general.action";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const HomePage = () => {
+
+const HomePage = async () => {
+  const user = await getCurrentUser();
+  const [userInterviews, latestInterviews] = await Promise.all([
+    getInterviewByUserId(user?.id || ""),
+    getLatestInterviews({ userId: user?.id || "", limit: 10 }),
+  ]);
+  const hasPastInterviews = (userInterviews?.length || 0) > 0;
+  const hasUpcomingInterviews = (latestInterviews?.length || 0) > 0;
   return (
     <>
       <section className="card-cta">
@@ -30,18 +42,25 @@ const HomePage = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          {dummyInterviews?.map((interview) => {
-            return <InterviewCard key={interview?.id} {...interview} />;
-          })}
-          {/* {dummyInterviews?.length === 0 && (
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => {
+              return <InterviewCard key={interview?.id} {...interview} />;
+            })
+          ) : (
             <p>You haven&apos;t taken any interviews yet</p>
-          )} */}
+          )}
         </div>
       </section>
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
         <div className="interviews-section">
-          <p>There are no interviews available</p>
+          {hasUpcomingInterviews ? (
+            latestInterviews?.map((interview) => {
+              return <InterviewCard key={interview?.id} {...interview} />;
+            })
+          ) : (
+            <p>There are no interviews available</p>
+          )}
         </div>
       </section>
     </>
---
+++ lib/actions/general.action.ts
@@ -0,0 +1,37 @@
+import { db } from "@/firebase/admin";
+import { GetLatestInterviewsParams, Interview } from "@/types";
+
+export async function getInterviewByUserId(userId: string): Promise<Interview[] | null> {
+  const interviews = await db.collection("interviews").where("userId", "==", userId).orderBy("createdAt", "desc").get();
+
+  return interviews.docs.map((doc) => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+    };
+  }) as Interview[];
+
+
+}
+
+// handler to get the interviews created by other users...
+
+
+export async function getLatestInterviews(
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+  const { userId, limit = 10 } = params;
+  
+  const interviews = await db
+    .collection("interviews")
+    .orderBy("createdAt", "desc")
+    .where("finalized", "==", true).where("user_id", "!=", userId).limit(limit)
+    .get();
+
+  return interviews.docs.map((doc) => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+    };
+  }) as Interview[];
+}
